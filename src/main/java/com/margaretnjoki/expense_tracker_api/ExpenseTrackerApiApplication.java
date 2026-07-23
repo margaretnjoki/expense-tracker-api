c81709bde6f454bb75a91f0987f875c7b028f240
margaretnjoki <maggykiroko@gmail.com>
2026-07-23 16:16:06 +0300

    add log in the main program to know when the program starts,,,

--- src/main/java/com/margaretnjoki/expense_tracker_api/ExpenseTrackerApiApplication.java
+++ src/main/java/com/margaretnjoki/expense_tracker_api/ExpenseTrackerApiApplication.java
@@ -1,13 +1,16 @@
 package com.margaretnjoki.expense_tracker_api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+		log.info("Expense Tracker API Version 2.1 started");
+
 	}
 
 }
