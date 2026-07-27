76d1e4d7e29a04354e46192f87fc174d6d5c53b9
margaretnjoki <maggykiroko@gmail.com>
2026-07-27 06:32:33 +0300

    comment  @springTest

--- src/test/java/com/margaretnjoki/expense_tracker_api/ExpenseTrackerApiApplicationTests.java
+++ src/test/java/com/margaretnjoki/expense_tracker_api/ExpenseTrackerApiApplicationTests.java
@@ -3,11 +3,11 @@ package com.margaretnjoki.expense_tracker_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ExpenseTrackerApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ExpenseTrackerApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
